4045938b4eeff347c3112b263e6dd4e51d1e555e
WilliamWilsonNSCC <w0457754@campus.nscc.ca>
2026-04-20 19:26:29 -0300

    Started building what the application is for

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>THIS APP IS GOING TO HELP TRACK</Text>
+      <Text>BLOOD PRESSURE AND BLOOD SUGAR LEVELS</Text>
       <StatusBar style="auto" />
     </View>
   );
